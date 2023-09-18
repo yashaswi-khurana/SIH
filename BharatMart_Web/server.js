a6a0898b8793b48d969ee22befa815ffb1adb860
Anubhavpandey27 <anubhavpandey.bt22cseds@pec.edu.in>
2023-09-18 23:28:16 +0530

    update

--- BharatMart_Web/server.js
+++ BharatMart_Web/server.js
@@ -5,7 +5,31 @@ app.set('view engine','ejs')
 app.use(express.static(path.join(__dirname,'public')))
 app.get('/',(req,res)=>{
     console.log("hello")
+    res.render('index.ejs')
+    
+   
+})
+app.get('/home',(req,res)=>{
+    console.log("hello")
+    res.render('index.ejs')
+    
+   
+})
+app.get('/profile',(req,res)=>{
+    console.log("hello")
     res.render('login.ejs')
+    
+   
+})
+app.get('/update',(req,res)=>{
+    console.log("hello")
+    res.render('removeitem.ejs')
+    
+   
+})
+app.get('/salesrecord',(req,res)=>{
+    console.log("hello")
+    res.render('salesrecord.ejs')
     
    
 })
