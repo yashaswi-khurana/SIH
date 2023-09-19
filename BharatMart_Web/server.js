9f0608b5a7ba53eebeae8bc1b7b9a37f49396c42
coderiders22 <119496654+coderiders22@users.noreply.github.com>
2023-09-19 14:51:06 +0530

    Updated

--- BharatMart_Web/server.js
+++ BharatMart_Web/server.js
@@ -1,16 +1,13 @@
 const express=require('express');
 const Database=require('./database/database.js')
 const config = require('./database/config.js')
-
 const path=require('path')
-
 
 const database = new Database(config);
 
 database
     .executeQuery(
-      `CREATE TABLE Vendors (Vendor_ID int NOT NULL PRIMARY KEY, Vendor_Name varchar(30));`
-      //Example query
+      `CREATE TABLE Login (id int NOT NULL IDENTITY, passwd varchar(30));`
     )
     .then(() => {
       console.log('Table created');
@@ -68,5 +65,3 @@ app.get('/signup',(req,res)=>{
 
 app.listen(3000);
 
-
-const loginRoute = require('./database/routes/login.js')
