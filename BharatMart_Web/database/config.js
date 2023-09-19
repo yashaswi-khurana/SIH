e5fe899b6b593124cc13f4f1c0c29743087285e3
Parth Dhiman <cynical.melomaniac@gmail.com>
2023-09-19 07:39:11 +0530

    Adding Vendor functions to website

--- BharatMart_Web/database/config.js
+++ BharatMart_Web/database/config.js
@@ -1,7 +1,7 @@
-const server = process.env.AZURE_SQL_SERVER;
-const database = process.env.AZURE_SQL_DATABASE;
-const port = parseInt(process.env.AZURE_SQL_PORT);
-const type = process.env.AZURE_SQL_AUTHENTICATIONTYPE;
+const server = 'bharatmart-sih.database.windows.net';
+const database = 'bharatmart';
+const port = 1433;
+const type = 'azure-active-directory-default';
 
 const config = {
     server,
